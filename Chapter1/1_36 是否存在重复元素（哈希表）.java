e40d0a5a82bb2c70e0df84c3080226ce734de7db
Eason Lin <2073043435@qq.com>
2026-03-08 18:07:31 +0800

    提交了1_36的两种解法，当数组内元素大小范围不清楚时同样适用。

---
+++ Chapter1/1_36 是否存在重复元素（哈希表）.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+    P46 1.37
+    考虑元素唯一性问题：给出一个整数集合，甲丁这些整数存储在数组A[1...n]中，
+    确定其中书否存在两个相等的元素。请设计出一个有效算法来解决这个问题，你的算法的时间复杂性是多少？
+
+    本解答运用哈希表，创建一个哈希集合来存放已经出现过的元素。
+    时间复杂性O(n)  空间复杂性O(n)
+*/
+
+class Solution {
+    public static boolean hasRepeatElements(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
---
+++ Chapter1/1_36 是否存在重复元素（排序）.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/*
+    P46 1.37
+    考虑元素唯一性问题：给出一个整数集合，甲丁这些整数存储在数组A[1...n]中，
+    确定其中书否存在两个相等的元素。请设计出一个有效算法来解决这个问题，你的算法的时间复杂性是多少？
+
+    本解答运用排序+线性遍历
+    时间复杂性O(nlogn)  空间复杂性O(1)
+*/
+
+class Solution {
+    public static boolean hasRepeatElements(int[] nums) {
+        Arrays.sort(nums); // 使用库函数的快速排序先将nums升序排列
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
