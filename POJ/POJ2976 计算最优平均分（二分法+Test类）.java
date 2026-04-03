f70a891ce2ccaa40b543c1c2012a4496e854f7fb
Eason Lin <2073043435@qq.com>
2026-04-03 21:25:45 +0800

    将POJ2976封装入Test类

---
+++ POJ/POJ2976 计算最优平均分（二分法+Test类）.java
@@ -0,0 +1,28 @@
+class Test implements Comparable<Test> {
+    int correct;
+    int total;
+    double avg;
+
+    Test() {
+        this.correct = 0;
+        this.total = 0;
+        this.avg = 0;
+    }
+
+    static Test[] getTests(int len, Scanner input) {
+        Test[] tests = new Test[len];
+        for (int i = 0; i < len; i++) {
+            tests[i] = new Test();
+            tests[i].correct = input.nextInt();
+        }
+        for (int i = 0; i < len; i++) {
+            tests[i].total = input.nextInt();
+        }
+        return tests;
+    }
+
+    @Override
+    public int compareTo(Test t) {
+        return Double.compare(this.correct - this.total * this.avg, t.correct - t.total * t.avg);
+    }
+}
